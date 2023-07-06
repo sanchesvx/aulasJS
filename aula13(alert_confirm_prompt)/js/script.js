3e380674e34598d7fdceee6baf2735b73c4f5c07
sanchesvx <victor.sm1208@gmail.com>
2023-07-06 13:33:20 -0300

    commit

---
+++ aula13(alert_confirm_prompt)/js/script.js
@@ -0,0 +1,7 @@
+let num1 = prompt('Digite um número:');
+let num2 = prompt('Digite outro número:');
+
+num1 = Number(num1); //tranformar string em number
+num2 = Number(num2);
+
+alert(`O resultado da sua conta foi: ${num1 + num2}`);
