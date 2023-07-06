9c241a3f05771d4355014f39191b4e091a535d0c
sanchesvx <victor.sm1208@gmail.com>
2023-07-06 13:28:37 -0300

    commit

---
+++ aula11(dados_primitivos)/index.js
@@ -0,0 +1,10 @@
+// String, Number, Undefined, Null, Boolean, Symbol(verá no futuro)
+const nome = 'Vi'; //String
+const nome1 = 'Vi'; //String
+const nome2 = 'Vi'; //String
+const num1 = '10'; //Number
+const num2 = '10.52'; //Number
+let nomeAluno;  //Undefined -> não aponta para local nenhum na memória
+let sobrenomeAluno = null; // Nulo: não aponta para local nenhum na memória
+const aprovado = false; // Boolean = True or False (lógico)
+
