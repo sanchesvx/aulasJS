c02280a109b7a1d7b61230a2ee7999aea0f86c97
sanchesvx <victor.sm1208@gmail.com>
2023-07-07 13:37:16 -0300

    commit

---
+++ aula18(math)/index.js
@@ -0,0 +1,21 @@
+let num1 = 9.54578;
+let num2 = Math.floor(num1); //arredonda pra baixo
+let num3 = Math.ceil(num1);  //arredonda pra cima
+console.log(num2);
+console.log(num3);
+
+let num4 = Math.round(num1); //arredonda pro mais próximo
+console.log(num4)
+
+console.log(Math.max(1,2,3,4,5,-10,-50,1500,9,8,7,6)); //pega o maior número
+console.log(Math.min(1,2,3,4,5,-10,-50,1500,9,8,7,6)); //pega o menor número
+
+const aleatorio = Math.round(Math.random() *(10 - 5) + 5);
+console.log(aleatorio);
+
+console.log(Math.PI)
+
+let num5 = 9;
+console.log(num5 ** 0.5);
+
+console.log(100 / 0)
