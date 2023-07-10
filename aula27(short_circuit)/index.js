f16afabd8fc2a78989ec6daefea3ac4b10be3306
sanchesvx <victor.sm1208@gmail.com>
2023-07-10 14:43:22 -0300

    commit

---
+++ aula27(short_circuit)/index.js
@@ -0,0 +1,17 @@
+/*
+&& false && true -> false
+|| false || true -> true
+
+
+FALSY VALUES
+
+false
+0
+'' "" ``
+null / undefined
+NaN
+
+*/
+
+console.log('Vi' && 0 && 'San');
+console.log(null || 0 || undefined || 'Vi' || false);
