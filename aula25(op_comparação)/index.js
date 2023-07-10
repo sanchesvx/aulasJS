7b01b2959e98233b9e345201b9aa94de416dbe38
sanchesvx <victor.sm1208@gmail.com>
2023-07-10 14:42:11 -0300

    commit

---
+++ aula25(op_comparação)/index.js
@@ -0,0 +1,23 @@
+/*
+Operadores de comparação
+>   maior
+>=  maior ou igual
+<   menor
+<=  menor ou igual
+==  igualdade (valor) 
+=== igualdade estrita (valor e tipo) <--optar sempre por ele
+!=  diferente (valor)
+!== diferente estrito (valor e tipo) <--optar sempre por ele
+*/
+
+const comp = 10 == '10'
+console.log(comp)
+
+const comp1 = 10 === '10'
+console.log(comp1)
+
+const comp2 = 10 != '10'
+console.log(comp2)
+
+const comp3 = 10 !== '10'
+console.log(comp3)
