b649907d4ffc3a0da4f6181b7a285e2531cf2e37
sanchesvx <victor.sm1208@gmail.com>
2023-07-19 00:04:06 -0300

    commit

---
+++ aula56(return_func)/index.js
@@ -0,0 +1,14 @@
+// return = retorna um valor e termina a função
+function criaMultiplicador(multiplicador) {
+    return function(n) {
+        return n * multiplicador;
+    };
+}
+
+const duplica = criaMultiplicador(2);
+const triplica = criaMultiplicador(3);
+const quadriplica = criaMultiplicador(4);
+
+console.log(duplica(3))
+console.log(triplica(2))
+console.log(quadriplica(30))
